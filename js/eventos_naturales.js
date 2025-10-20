1acce123e88c245facbcf860b46ebb8373645911
maricielo-hc <maricielo.mr04@gmail.com>
2025-10-20 06:18:53 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -1,139 +1,6 @@
 const eventosNaturales = [
   {
     "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "02:32",
-    "lat": -3.98616,
-    "lon": -79.39204
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "02:34",
-    "lat": -1.33772,
-    "lon": -79.91117
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:14",
-    "lat": -11.20773,
-    "lon": -73.17648
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:14",
-    "lat": -10.8645,
-    "lon": -73.1978
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:17",
-    "lat": -9.17513,
-    "lon": -75.00903
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:17",
-    "lat": -8.32724,
-    "lon": -75.1641
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:17",
-    "lat": -7.69182,
-    "lon": -79.36376
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:17",
-    "lat": -6.58791,
-    "lon": -79.82741
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:17",
-    "lat": -4.09825,
-    "lon": -79.42712
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:19",
-    "lat": -1.98394,
-    "lon": -79.86129
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:19",
-    "lat": -1.83626,
-    "lon": -79.93096
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:19",
-    "lat": -1.71077,
-    "lon": -79.83116
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:19",
-    "lat": -1.62828,
-    "lon": -79.95594
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:19",
-    "lat": -1.35622,
-    "lon": -80.43861
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:19",
-    "lat": -1.35749,
-    "lon": -80.44731
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:19",
-    "lat": -0.90031,
-    "lon": -80.4834
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:19",
-    "lat": -0.8474,
-    "lon": -80.44346
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:19",
-    "lat": -0.57034,
-    "lon": -80.36304
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2025-10-18",
-    "hora": "20:19",
-    "lat": -0.57161,
-    "lon": -80.37174
-  },
-  {
-    "tipo": "Incendio",
     "fecha": "2025-10-19",
     "hora": "01:32",
     "lat": -11.26627,
@@ -152,6 +19,14 @@ const eventosNaturales = [
     "hora": "13:57",
     "lat": -11.83796,
     "lon": -76.47755
+  },
+  {
+    "tipo": "Sismo",
+    "fecha": "2025-10-18T23:16:10.684Z",
+    "lugar": "20 km WNW of Ilabaya, Peru",
+    "magnitud": "4.5",
+    "lat": -17.366,
+    "lon": -70.6933
   },
   {
     "tipo": "Sismo",
