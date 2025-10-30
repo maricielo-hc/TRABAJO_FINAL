9bb901dffbb220fe82540592f75dd2f58590db18
maricielo-hc <maricielo.mr04@gmail.com>
2025-10-30 18:16:46 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -7,6 +7,20 @@ const eventosNaturales = [
     "lon": -70.42971
   },
   {
+    "tipo": "Incendio",
+    "fecha": "2025-10-30",
+    "hora": "14:35",
+    "lat": -4.06586,
+    "lon": -78.95248
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-10-30",
+    "hora": "14:37",
+    "lat": -11.07799,
+    "lon": -77.46323
+  },
+  {
     "tipo": "Sismo",
     "fecha": "2025-10-25T22:09:36.148Z",
     "lugar": "87 km S of Pacocha, Peru",
