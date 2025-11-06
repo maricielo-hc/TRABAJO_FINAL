8087c7de9503b1a0f557aeb673af185783215431
maricielo-hc <maricielo.mr04@gmail.com>
2025-11-06 18:18:31 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -21,6 +21,55 @@ const eventosNaturales = [
     "lon": -68.49016
   },
   {
+    "tipo": "Incendio",
+    "fecha": "2025-11-06",
+    "hora": "14:16",
+    "lat": -6.66373,
+    "lon": -79.77497
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-11-06",
+    "hora": "14:16",
+    "lat": -11.05799,
+    "lon": -75.32735
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-11-06",
+    "hora": "14:16",
+    "lat": -11.12469,
+    "lon": -75.36593
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-11-06",
+    "hora": "14:16",
+    "lat": -11.1262,
+    "lon": -75.35458
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-11-06",
+    "hora": "14:16",
+    "lat": -13.42412,
+    "lon": -73.80582
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-11-06",
+    "hora": "14:16",
+    "lat": -13.63871,
+    "lon": -72.88404
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-11-06",
+    "hora": "14:16",
+    "lat": -13.69832,
+    "lon": -73.71153
+  },
+  {
     "tipo": "Sismo",
     "fecha": "2025-11-04T07:14:13.207Z",
     "lugar": "70 km S of Alianza Cristiana, Peru",
