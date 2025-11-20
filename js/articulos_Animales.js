177546a981f51f301fa48098fe8f9bf598630559
Dhalia <alfarodomini@gmail.com>
2025-11-20 01:14:45 +0000

    Auto-update artículos (via GitHub Actions)

--- js/articulos_Animales.js
+++ js/articulos_Animales.js
@@ -7026,7 +7026,7 @@ const articulos = [
     "url": "https://es.mongabay.com/2016/07/el-misterioso-yaguarundi/"
   },
   {
-    "titulo": "Última actualización automática - Wed Nov 19 18:27:42 2025",
+    "titulo": "Última actualización automática - Thu Nov 20 01:14:45 2025",
     "imagen": "",
     "fecha": "",
     "url": "#"
