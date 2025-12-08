bd04473b095023d26f686c719542ee4f69084ef8
maricielo-hc <maricielo.mr04@gmail.com>
2025-12-08 18:19:41 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -392,6 +392,118 @@ const eventosNaturales = [
     "lon": -71.34969
   },
   {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -6.0077,
+    "lon": -77.63878
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -6.04133,
+    "lon": -77.4948
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -6.04387,
+    "lon": -77.55266
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -6.04603,
+    "lon": -77.53851
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -6.11745,
+    "lon": -77.52576
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -6.11982,
+    "lon": -77.51045
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -6.07619,
+    "lon": -78.02138
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -6.0877,
+    "lon": -78.0237
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -6.08991,
+    "lon": -78.00918
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -6.06379,
+    "lon": -78.70302
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -7.0293,
+    "lon": -77.7376
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:57",
+    "lat": -13.42255,
+    "lon": -74.11082
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:57",
+    "lat": -13.42383,
+    "lon": -74.10187
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:57",
+    "lat": -13.67714,
+    "lon": -74.20297
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -1.36071,
+    "lon": -78.73523
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-08",
+    "hora": "13:54",
+    "lat": -4.19219,
+    "lon": -80.12463
+  },
+  {
     "tipo": "Sismo",
     "fecha": "2025-12-07T23:49:16.613Z",
     "lugar": "2 km NNW of Chipispaya, Peru",
