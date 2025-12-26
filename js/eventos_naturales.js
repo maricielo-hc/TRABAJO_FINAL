9fb0ccae17ad5fec099384ef4da42b0ca2d662ef
maricielo-hc <maricielo.mr04@gmail.com>
2025-12-26 18:18:43 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -49,6 +49,196 @@ const eventosNaturales = [
     "lon": -75.82105
   },
   {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "08:00",
+    "lat": -0.07569,
+    "lon": -77.66087
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "08:00",
+    "lat": -2.00286,
+    "lon": -78.34194
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -9.93197,
+    "lon": -76.51807
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -10.22686,
+    "lon": -76.77742
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -11.26503,
+    "lon": -75.71574
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -11.11338,
+    "lon": -77.36752
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -13.13329,
+    "lon": -75.85722
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -13.53153,
+    "lon": -74.3141
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -13.53358,
+    "lon": -74.29727
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -13.66666,
+    "lon": -73.40163
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:12",
+    "lat": -2.004,
+    "lon": -78.34142
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -2.47788,
+    "lon": -79.51852
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -3.57851,
+    "lon": -80.528
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -3.58014,
+    "lon": -80.51676
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -4.1647,
+    "lon": -79.96561
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -4.15023,
+    "lon": -80.33046
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -4.17119,
+    "lon": -80.32247
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -4.17737,
+    "lon": -80.27978
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -4.71124,
+    "lon": -80.27618
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -5.0671,
+    "lon": -78.97294
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -5.6966,
+    "lon": -79.90903
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -6.58264,
+    "lon": -79.77253
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -6.58406,
+    "lon": -79.76296
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -7.8364,
+    "lon": -79.2132
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -8.16776,
+    "lon": -77.02624
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2025-12-26",
+    "hora": "14:14",
+    "lat": -8.03888,
+    "lon": -79.08892
+  },
+  {
+    "tipo": "Sismo",
+    "fecha": "2025-12-26T16:31:39.355Z",
+    "lugar": "42 km W of Campo Verde, Peru",
+    "magnitud": "4.8",
+    "lat": -8.4377,
+    "lon": -75.1957
+  },
+  {
     "tipo": "Sismo",
     "fecha": "2025-12-25T06:46:03.965Z",
     "lugar": "51 km WSW of Atiquipa, Peru",
