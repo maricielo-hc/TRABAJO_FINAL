0142016e25b5623179073f07926fd6ccb6055e13
maricielo-hc <maricielo.mr04@gmail.com>
2026-01-23 18:20:50 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -1,5 +1,48 @@
 const eventosNaturales = [
   {
+    "tipo": "Incendio",
+    "fecha": "2026-01-23",
+    "hora": "14:31",
+    "lat": -7.77715,
+    "lon": -79.16344
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-01-23",
+    "hora": "14:31",
+    "lat": -7.77891,
+    "lon": -79.14992
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-01-23",
+    "hora": "14:31",
+    "lat": -7.85621,
+    "lon": -79.26103
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-01-23",
+    "hora": "14:31",
+    "lat": -8.34822,
+    "lon": -78.73565
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-01-23",
+    "hora": "14:31",
+    "lat": -8.3503,
+    "lon": -78.72005
+  },
+  {
+    "tipo": "Sismo",
+    "fecha": "2026-01-23T09:27:27.219Z",
+    "lugar": "80 km SE of Palca, Peru",
+    "magnitud": "4.4",
+    "lat": -18.293,
+    "lon": -69.4249
+  },
+  {
     "tipo": "Sismo",
     "fecha": "2026-01-16T21:15:06.513Z",
     "lugar": "92 km SE of Yantzaza, Ecuador",
