b48738a441a7fa0359bbb785dec57d3a189703ec
maricielo-hc <maricielo.mr04@gmail.com>
2026-01-26 12:29:23 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -29,6 +29,14 @@ const eventosNaturales = [
   },
   {
     "tipo": "Sismo",
+    "fecha": "2026-01-26T06:31:50.139Z",
+    "lugar": "12 km S of Santa Lucía, Peru",
+    "magnitud": "4.2",
+    "lat": -15.8142,
+    "lon": -70.6039
+  },
+  {
+    "tipo": "Sismo",
     "fecha": "2026-01-25T20:29:01.866Z",
     "lugar": "16 km NNE of Honoria, Peru",
     "magnitud": "4.6",
