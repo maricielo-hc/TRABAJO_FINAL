85ae0df166e89bfde0b1cda74e7619eec58a951e
maricielo-hc <maricielo.mr04@gmail.com>
2026-02-03 01:17:27 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -14,6 +14,34 @@ const eventosNaturales = [
     "lon": -71.80067
   },
   {
+    "tipo": "Incendio",
+    "fecha": "2026-02-02",
+    "hora": "19:59",
+    "lat": -13.77121,
+    "lon": -68.02693
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-02-02",
+    "hora": "19:59",
+    "lat": -13.77278,
+    "lon": -68.03704
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-02-02",
+    "hora": "19:59",
+    "lat": -11.58725,
+    "lon": -73.15974
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-02-02",
+    "hora": "19:59",
+    "lat": -11.5887,
+    "lon": -73.1701
+  },
+  {
     "tipo": "Sismo",
     "fecha": "2026-01-31T01:09:18.050Z",
     "lugar": "50 km SSE of San Juan, Peru",
