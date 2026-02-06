8be78c1d398213f6a2eac18bad4bd8bd6fb95af9
maricielo-hc <maricielo.mr04@gmail.com>
2026-02-06 12:38:01 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -322,6 +322,20 @@ const eventosNaturales = [
     "lon": -78.34332
   },
   {
+    "tipo": "Incendio",
+    "fecha": "2026-02-06",
+    "hora": "08:39",
+    "lat": -1.99938,
+    "lon": -78.33401
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-02-06",
+    "hora": "08:39",
+    "lat": -2.01,
+    "lon": -78.3353
+  },
+  {
     "tipo": "Sismo",
     "fecha": "2026-02-03T02:08:18.331Z",
     "lugar": "10 km NNE of Santa Lucía, Peru",
