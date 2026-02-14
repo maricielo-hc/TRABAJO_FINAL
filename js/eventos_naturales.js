6b716d5a160c7bf58de989cf7eb052c4209c2dcb
maricielo-hc <maricielo.mr04@gmail.com>
2026-02-14 18:21:41 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -20,13 +20,5 @@ const eventosNaturales = [
     "magnitud": "4.6",
     "lat": -2.4075,
     "lon": -77.6976
-  },
-  {
-    "tipo": "Sismo",
-    "fecha": "2026-02-07T12:37:46.480Z",
-    "lugar": "7 km ENE of Ilabaya, Peru",
-    "magnitud": "4.4",
-    "lat": -17.386,
-    "lon": -70.448
   }
 ];
