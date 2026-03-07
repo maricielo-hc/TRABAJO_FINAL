7d3de13577dc4f52c92f7f25ac389d9efa2adb5e
Dhalia <alfarodomini@gmail.com>
2026-03-07 18:31:49 +0000

    Auto-update artículos (via GitHub Actions)

--- js/articulos_Animales.js
+++ js/articulos_Animales.js
@@ -1,5 +1,11 @@
 const articulos = [
   {
+    "titulo": "Ciencia hecha por mujeres: investigaciones clave para entender y proteger los ecosistemas de América Latina",
+    "imagen": "https://imgs.mongabay.com/wp-content/uploads/sites/25/2025/07/30023303/Equipo-con-ballena-Aurum_credito-Paula-Faiferman-Siguiendoballenas-768x512.jpg",
+    "fecha": "Mar 07, 2026",
+    "url": "https://es.mongabay.com/2026/03/mujeres-ciencia-conservacion-biodiversidad-america-latina/"
+  },
+  {
     "titulo": "Pez guitarra: científicos y pescadores deportivos de Argentina se unen para proteger a esta singular especie del Atlántico",
     "imagen": "https://imgs.mongabay.com/wp-content/uploads/sites/25/2026/02/26012519/Captura-de-pantalla-2026-02-25-a-las-6.24.17-p.m-768x512.png",
     "fecha": "Feb 27, 2026",
@@ -7344,7 +7350,7 @@ const articulos = [
     "url": "https://es.mongabay.com/2016/07/el-misterioso-yaguarundi/"
   },
   {
-    "titulo": "Última actualización automática - Sat Mar  7 12:41:18 2026",
+    "titulo": "Última actualización automática - Sat Mar  7 18:31:48 2026",
     "imagen": "",
     "fecha": "",
     "url": "#"
