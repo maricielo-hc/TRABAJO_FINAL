38d4d6bdaefc392f7fe48124d047e0fc4533576b
Dhalia <alfarodomini@gmail.com>
2026-03-23 18:50:58 +0000

    Auto-update artículos (via GitHub Actions)

--- js/articulos_Animales.js
+++ js/articulos_Animales.js
@@ -1,5 +1,11 @@
 const articulos = [
   {
+    "titulo": "Investigadores awá: el equipo que une ciencia y ancestralidad para proteger la selva en Colombia",
+    "imagen": "https://imgs.mongabay.com/wp-content/uploads/sites/25/2026/03/23050349/Captura-de-pantalla-2026-03-22-a-las-10.02.18-p.m-768x512.png",
+    "fecha": "Mar 23, 2026",
+    "url": "https://es.mongabay.com/2026/03/investigadores-awa-ciencia-ancestralidad-proteger-selva-colombia/"
+  },
+  {
     "titulo": "Perú: la pesca con palangre amenaza a los albatros, pero científicos buscan soluciones",
     "imagen": "https://imgs.mongabay.com/wp-content/uploads/sites/25/2026/03/18224656/IMG_3753-salv-bait-768x512.jpg",
     "fecha": "Mar 20, 2026",
@@ -7374,7 +7380,7 @@ const articulos = [
     "url": "https://es.mongabay.com/2016/07/el-misterioso-yaguarundi/"
   },
   {
-    "titulo": "Última actualización automática - Mon Mar 23 13:00:58 2026",
+    "titulo": "Última actualización automática - Mon Mar 23 18:50:58 2026",
     "imagen": "",
     "fecha": "",
     "url": "#"
