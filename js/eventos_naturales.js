47bc1b0222d5933c22f371c6026b6204d7adfe40
maricielo-hc <maricielo.mr04@gmail.com>
2026-04-01 01:46:54 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -7,6 +7,76 @@ const eventosNaturales = [
     "lon": -78.57127
   },
   {
+    "tipo": "Incendio",
+    "fecha": "2026-03-31",
+    "hora": "20:14",
+    "lat": -16.5747,
+    "lon": -72.8168
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-03-31",
+    "hora": "20:17",
+    "lat": -8.41096,
+    "lon": -78.82962
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-03-31",
+    "hora": "20:19",
+    "lat": -7.88807,
+    "lon": -79.20825
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-03-31",
+    "hora": "20:19",
+    "lat": -6.81992,
+    "lon": -79.76878
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-03-31",
+    "hora": "20:19",
+    "lat": -6.05891,
+    "lon": -76.87365
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-03-31",
+    "hora": "20:19",
+    "lat": -5.33031,
+    "lon": -79.88448
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-03-31",
+    "hora": "20:19",
+    "lat": -5.10578,
+    "lon": -78.78937
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-03-31",
+    "hora": "20:19",
+    "lat": -4.88117,
+    "lon": -78.00518
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-03-31",
+    "hora": "20:19",
+    "lat": -4.90483,
+    "lon": -78.31903
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-03-31",
+    "hora": "20:19",
+    "lat": -4.46251,
+    "lon": -76.05756
+  },
+  {
     "tipo": "Sismo",
     "fecha": "2026-03-27T18:55:04.013Z",
     "lugar": "41 km ENE of Palca, Peru",
