1759f22a30b3cd3c2d47a82ddd21b48de22b9a11
maricielo-hc <maricielo.mr04@gmail.com>
2026-04-02 12:51:37 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -1,83 +1,6 @@
 const eventosNaturales = [
   {
     "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "14:03",
-    "lat": -7.45102,
-    "lon": -78.57127
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "20:14",
-    "lat": -16.5747,
-    "lon": -72.8168
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "20:17",
-    "lat": -8.41096,
-    "lon": -78.82962
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "20:19",
-    "lat": -7.88807,
-    "lon": -79.20825
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "20:19",
-    "lat": -6.81992,
-    "lon": -79.76878
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "20:19",
-    "lat": -6.05891,
-    "lon": -76.87365
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "20:19",
-    "lat": -5.33031,
-    "lon": -79.88448
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "20:19",
-    "lat": -5.10578,
-    "lon": -78.78937
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "20:19",
-    "lat": -4.88117,
-    "lon": -78.00518
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "20:19",
-    "lat": -4.90483,
-    "lon": -78.31903
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-03-31",
-    "hora": "20:19",
-    "lat": -4.46251,
-    "lon": -76.05756
-  },
-  {
-    "tipo": "Incendio",
     "fecha": "2026-04-01",
     "hora": "08:39",
     "lat": -0.08389,
@@ -159,6 +82,13 @@ const eventosNaturales = [
     "hora": "20:57",
     "lat": -7.81932,
     "lon": -79.17433
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-04-02",
+    "hora": "07:44",
+    "lat": -17.49333,
+    "lon": -71.34863
   },
   {
     "tipo": "Sismo",
