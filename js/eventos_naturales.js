3bf5cfe31859aacd0a2f695dce484f4adc8d5c96
maricielo-hc <maricielo.mr04@gmail.com>
2026-04-07 18:50:33 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -70,6 +70,20 @@ const eventosNaturales = [
     "lon": -79.23703
   },
   {
+    "tipo": "Incendio",
+    "fecha": "2026-04-07",
+    "hora": "13:44",
+    "lat": -13.7909,
+    "lon": -76.16803
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-04-07",
+    "hora": "13:44",
+    "lat": -17.49462,
+    "lon": -68.317
+  },
+  {
     "tipo": "Sismo",
     "fecha": "2026-04-04T12:23:28.453Z",
     "lugar": "39 km N of Playas, Ecuador",
