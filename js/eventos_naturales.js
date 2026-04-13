5b9110acc9adc4f3d657ce7e0b69ff5dff9e2f2b
maricielo-hc <maricielo.mr04@gmail.com>
2026-04-13 07:48:26 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -1,90 +1,6 @@
 const eventosNaturales = [
   {
     "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "02:19",
-    "lat": -1.99965,
-    "lon": -78.34431
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "08:30",
-    "lat": -9.1736,
-    "lon": -78.44002
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "08:33",
-    "lat": -13.78132,
-    "lon": -76.16666
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "14:42",
-    "lat": -6.8156,
-    "lon": -79.57127
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "14:42",
-    "lat": -6.81816,
-    "lon": -79.57687
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "14:40",
-    "lat": -0.70557,
-    "lon": -80.09754
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "20:46",
-    "lat": -2.32766,
-    "lon": -79.69916
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "20:46",
-    "lat": -2.34034,
-    "lon": -80.09426
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "20:44",
-    "lat": -7.76431,
-    "lon": -79.28978
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "20:44",
-    "lat": -7.31844,
-    "lon": -79.32711
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "20:44",
-    "lat": -7.21916,
-    "lon": -79.47426
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-04-11",
-    "hora": "20:44",
-    "lat": -7.22049,
-    "lon": -79.48325
-  },
-  {
-    "tipo": "Incendio",
     "fecha": "2026-04-12",
     "hora": "19:42",
     "lat": -17.66916,
@@ -96,6 +12,13 @@ const eventosNaturales = [
     "hora": "19:45",
     "lat": -4.23514,
     "lon": -69.92955
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-04-13",
+    "hora": "01:56",
+    "lat": -13.78543,
+    "lon": -76.1708
   },
   {
     "tipo": "Sismo",
