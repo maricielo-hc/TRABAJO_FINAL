4b0a48b15e4190400cb64795659bf1a2aac7487e
maricielo-hc <maricielo.mr04@gmail.com>
2026-05-19 19:42:39 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -43,18 +43,18 @@ const eventosNaturales = [
   },
   {
     "tipo": "Sismo",
+    "fecha": "2026-05-19T17:57:54.757Z",
+    "lugar": "25 km ESE of Pampa de Tate, Peru",
+    "magnitud": "5.9",
+    "lat": -14.2425,
+    "lon": -75.4795
+  },
+  {
+    "tipo": "Sismo",
     "fecha": "2026-05-15T15:45:18.856Z",
     "lugar": "11 km WNW of Palca, Peru",
     "magnitud": "4.9",
     "lat": -17.732,
     "lon": -70.0593
-  },
-  {
-    "tipo": "Sismo",
-    "fecha": "2026-05-12T18:32:39.115Z",
-    "lugar": "13 km S of Mazo Cruz, Peru",
-    "magnitud": "4.2",
-    "lat": -16.8629,
-    "lon": -69.7192
   }
 ];
