1f58e5a665d08496babea2f8e71adf1527d45222
maricielo-hc <maricielo.mr04@gmail.com>
2026-05-21 14:42:54 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -1,62 +1,6 @@
 const eventosNaturales = [
   {
     "tipo": "Incendio",
-    "fecha": "2026-05-19",
-    "hora": "20:19",
-    "lat": -2.10763,
-    "lon": -79.8225
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-05-19",
-    "hora": "20:19",
-    "lat": -2.10518,
-    "lon": -79.82989
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-05-19",
-    "hora": "20:17",
-    "lat": -17.60652,
-    "lon": -71.23792
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-05-19",
-    "hora": "20:17",
-    "lat": -17.60785,
-    "lon": -71.24723
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-05-19",
-    "hora": "20:17",
-    "lat": -13.90388,
-    "lon": -75.65846
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-05-19",
-    "hora": "20:17",
-    "lat": -12.69435,
-    "lon": -69.58086
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-05-19",
-    "hora": "20:17",
-    "lat": -12.66708,
-    "lon": -72.36984
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-05-19",
-    "hora": "20:19",
-    "lat": -7.8999,
-    "lon": -79.17374
-  },
-  {
-    "tipo": "Incendio",
     "fecha": "2026-05-20",
     "hora": "13:53",
     "lat": -0.08222,
@@ -173,6 +117,20 @@ const eventosNaturales = [
     "hora": "20:59",
     "lat": -1.44145,
     "lon": -79.6468
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-05-21",
+    "hora": "02:09",
+    "lat": -6.76271,
+    "lon": -79.73618
+  },
+  {
+    "tipo": "Incendio",
+    "fecha": "2026-05-21",
+    "hora": "02:09",
+    "lat": -6.76393,
+    "lon": -79.74522
   },
   {
     "tipo": "Sismo",
