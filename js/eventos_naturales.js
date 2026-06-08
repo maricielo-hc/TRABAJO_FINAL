8c18ad99fde1829dec84c091ae5b72cf2abf2853
maricielo-hc <maricielo.mr04@gmail.com>
2026-06-08 10:17:55 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -1,153 +1,6 @@
 const eventosNaturales = [
   {
     "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "13:33",
-    "lat": -13.79105,
-    "lon": -76.16858
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "13:33",
-    "lat": -13.81661,
-    "lon": -76.13609
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:14",
-    "lat": -9.30186,
-    "lon": -68.73468
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:14",
-    "lat": -9.20772,
-    "lon": -71.95358
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:14",
-    "lat": -8.65333,
-    "lon": -69.62907
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:14",
-    "lat": -8.65473,
-    "lon": -69.63837
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:17",
-    "lat": -8.60296,
-    "lon": -74.30333
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:17",
-    "lat": -8.43666,
-    "lon": -74.65103
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:17",
-    "lat": -8.4265,
-    "lon": -74.65247
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:17",
-    "lat": -7.75728,
-    "lon": -72.51756
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:17",
-    "lat": -7.31195,
-    "lon": -72.69872
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:17",
-    "lat": -7.30298,
-    "lon": -72.70004
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:17",
-    "lat": -7.76764,
-    "lon": -79.11196
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:17",
-    "lat": -7.77116,
-    "lon": -79.1386
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:17",
-    "lat": -6.63553,
-    "lon": -79.87043
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:17",
-    "lat": -6.63697,
-    "lon": -79.87479
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:14",
-    "lat": -16.59434,
-    "lon": -68.04215
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:14",
-    "lat": -16.58403,
-    "lon": -68.03426
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:14",
-    "lat": -16.58543,
-    "lon": -68.04339
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:14",
-    "lat": -15.19666,
-    "lon": -68.88685
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-06-06",
-    "hora": "20:14",
-    "lat": -13.77754,
-    "lon": -68.06413
-  },
-  {
-    "tipo": "Incendio",
     "fecha": "2026-06-07",
     "hora": "01:47",
     "lat": -7.81337,
@@ -244,10 +97,10 @@ const eventosNaturales = [
   },
   {
     "tipo": "Sismo",
-    "fecha": "2026-06-01T23:53:54.025Z",
-    "lugar": "17 km WNW of San Ramón, Peru",
-    "magnitud": "4.2",
-    "lat": -11.0466,
-    "lon": -75.5
+    "fecha": "2026-06-01T23:53:54.121Z",
+    "lugar": "19 km WNW of San Ramón, Peru",
+    "magnitud": "4.3",
+    "lat": -11.0526,
+    "lon": -75.5204
   }
 ];
