c123c643a895c0da0b2856e519f7380563961438
maricielo-hc <maricielo.mr04@gmail.com>
2026-09-19 09:58:24 +0000

    Auto-update eventos naturales (via GitHub Actions)

--- js/eventos_naturales.js
+++ js/eventos_naturales.js
@@ -1,230 +1,6 @@
 const eventosNaturales = [
   {
     "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "13:25",
-    "lat": -7.0218,
-    "lon": -78.72235
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "13:25",
-    "lat": -7.02494,
-    "lon": -78.70929
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "13:27",
-    "lat": -11.84126,
-    "lon": -72.94701
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:00",
-    "lat": -14.65159,
-    "lon": -68.37634
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:00",
-    "lat": -14.65414,
-    "lon": -68.39633
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:00",
-    "lat": -14.57885,
-    "lon": -68.48228
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:00",
-    "lat": -12.83689,
-    "lon": -69.52303
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:00",
-    "lat": -12.80371,
-    "lon": -69.57533
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:00",
-    "lat": -12.70854,
-    "lon": -70.42022
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:00",
-    "lat": -12.71528,
-    "lon": -70.42632
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:00",
-    "lat": -12.70153,
-    "lon": -70.42805
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:00",
-    "lat": -12.33885,
-    "lon": -69.20218
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:00",
-    "lat": -12.15844,
-    "lon": -69.12482
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -11.30076,
-    "lon": -69.347
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -11.30306,
-    "lon": -69.36552
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -11.30955,
-    "lon": -69.35249
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -11.09916,
-    "lon": -69.005
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -11.09294,
-    "lon": -69.01273
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -10.16074,
-    "lon": -69.35786
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -9.06312,
-    "lon": -69.60133
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -8.9987,
-    "lon": -69.39851
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -8.81001,
-    "lon": -69.54045
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -8.86074,
-    "lon": -70.82684
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -8.73416,
-    "lon": -70.9454
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -8.66022,
-    "lon": -70.68983
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -8.51187,
-    "lon": -70.85263
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -8.46375,
-    "lon": -70.793
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -8.46755,
-    "lon": -70.79752
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -8.29097,
-    "lon": -70.87177
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -8.29216,
-    "lon": -70.85464
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -7.37465,
-    "lon": -68.01573
-  },
-  {
-    "tipo": "Incendio",
-    "fecha": "2026-09-17",
-    "hora": "20:02",
-    "lat": -7.37638,
-    "lon": -68.02837
-  },
-  {
-    "tipo": "Incendio",
     "fecha": "2026-09-18",
     "hora": "14:01",
     "lat": -0.18394,
@@ -358,6 +134,14 @@ const eventosNaturales = [
   },
   {
     "tipo": "Sismo",
+    "fecha": "2026-09-19T07:12:27.058Z",
+    "lugar": "117 km NW of Barranca, Peru",
+    "magnitud": "4.9",
+    "lat": -4.0134,
+    "lon": -77.349
+  },
+  {
+    "tipo": "Sismo",
     "fecha": "2026-09-15T23:45:06.518Z",
     "lugar": "17 km WNW of Palora, Ecuador",
     "magnitud": "4.5",
@@ -379,13 +163,5 @@ const eventosNaturales = [
     "magnitud": "4.3",
     "lat": -17.2664,
     "lon": -70.3681
-  },
-  {
-    "tipo": "Sismo",
-    "fecha": "2026-09-12T08:46:38.186Z",
-    "lugar": "63 km WSW of Santiago, Peru",
-    "magnitud": "4.5",
-    "lat": -14.4855,
-    "lon": -76.2161
   }
 ];
